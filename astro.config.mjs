1cfc503514050bc492675e668021008b76b3f0c3
Kibru Demeke <kibdemta@gmail.com>
2022-11-19 17:40:41 -0500

    placeholder pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,14 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
 export default defineConfig({
-    site: 'https://dkibru.github.io',
-    base: '/',
+  site: 'https://dkibru.github.io',
+  base: '/',
+  integrations: [tailwind(), mdx()]
 });
---
+++ src/content/posts/lets-take-a-look-at-hooks.md
@@ -0,0 +1,11 @@
+---
+layout: "../../components/BlogPost.astro"
+title: "Lets talk about hooks"
+description: "The description goes here"
+pubDate: "Oct 07 2022"
+heroImage: "/images/p1i1.jpg"
+socialImage: "/images/p1i1.jpg"
+---
+
+lorem ipsum ...
+
