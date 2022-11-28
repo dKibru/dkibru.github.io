3ff7ecfb749d9b6bfb5f36278e1cd8888ed1e671
Kibru Demeke <kibdemta@gmail.com>
2022-11-28 06:55:12 -0500

    integrate react and svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,14 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
+import react from "@astrojs/react";
+
+// https://astro.build/config
+import svelte from "@astrojs/svelte";
+
+// https://astro.build/config
 export default defineConfig({
   site: 'https://dkibru.github.io',
   base: '/',
-  integrations: [tailwind(), mdx()]
+  integrations: [tailwind(), mdx(), react(), svelte()]
 });
